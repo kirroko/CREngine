94b7def64110f0c02ba76542c84e772a94f0c3d6
Lum Ko Sand <42332579+dnasok@users.noreply.github.com>
2024-09-20 16:19:10 +0800

    Integrate Math lib, Collision and Physics files

---
+++ Ukemochi-Engine/src/Ukemochi-Engine/Collision/Collision.cpp
@@ -0,0 +1,46 @@
+/* Start Header
+*****************************************************************/
+/*!
+\file       Collision.cpp
+\author     Lum Ko Sand, kosand.lum, 2301263
+\par        email: kosand.lum\@digipen.edu
+\date       Sept 11, 2024
+\brief      This file contains the definition of the Collision system.
+
+Copyright (C) 2024 DigiPen Institute of Technology.
+Reproduction or disclosure of this file or its contents without the
+prior written consent of DigiPen Institute of Technology is prohibited.
+*/
+/* End Header
+*******************************************************************/
+
+#include "PreCompile.h"
+#include "Collision.h"
+
+namespace Ukemochi
+{
+    void CheckCollisions()
+    {
+        //// if the entity has a box collider
+        //if (list_of_entities[i] has box collider)
+
+        //    if (CollisionIntersection_RectRect())
+        //    {
+        //        ResolveCollisions();
+        //    }
+
+        //if (CollisionIntersection_CircleAABB())
+        //{
+        //    ResolveCollisions();
+        //}
+
+        //if (CollisionIntersection_CircleCircle())
+        //{
+        //    ResolveCollisions();
+        //}
+    }
+
+    void ResolveCollisions()
+    {
+    }
+}
