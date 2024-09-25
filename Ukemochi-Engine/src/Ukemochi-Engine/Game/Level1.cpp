745612e20362d6e0b945ce15b6960916bdb87d60
Lum Ko Sand <42332579+dnasok@users.noreply.github.com>
2024-09-25 13:47:40 +0800

    Testing of systems in level 1

--- Ukemochi-Engine/src/Ukemochi-Engine/Game/Level1.cpp
+++ Ukemochi-Engine/src/Ukemochi-Engine/Game/Level1.cpp
@@ -16,6 +16,19 @@ Copyright (C) 2024 DigiPen Institute of Technology.  Reproduction or disclosure 
 #include "Level1.h"
 #include <iostream>
 
+#include "../Input.h"					// for user input
+#include "../Collision/Collision.h"		// for collision system
+#include "../Physics/Physics.h"			// for physics system
+//#include "../Graphics/Renderer.h"		// for render system
+
+#include "../Collision/BoxCollider2D.h" // for BoxCollider2D
+#include "../Physics/Rigidbody2D.h"		// for Rigidbody2D
+
+// --- TEMP player variables ---
+Ukemochi::BoxCollider2D player_box_collider;
+Ukemochi::Rigidbody2D player_rb;
+//Renderer renderer_system;
+
 void Level1_Load()//Load all necessary assets before start of Level1
 {
 	//some code here
@@ -32,12 +45,61 @@ void Level1_Update()//Level1 game runtime
 {
 	//some code here
 	//std::cout << "Level1:Update" << '\n';
+
+	// --- Handle User Input for Player controls ---
+	// Press 'W' or up key to move the player up
+	if (UME::Input::IsKeyPressed(UME_KEY_W) || UME::Input::IsKeyPressed(UME_KEY_UP))
+	{
+		player_rb.velocity.y = -player_rb.acceleration.y;
+		std::cout << "X: " << (int)player_rb.position_curr.x << ", Y: " << (int)player_rb.position_curr.y << '\n';
+	}
+	// Press 'S' or down key to move the player down
+	else if (UME::Input::IsKeyPressed(UME_KEY_S) || UME::Input::IsKeyPressed(UME_KEY_DOWN))
+	{
+		player_rb.velocity.y = player_rb.acceleration.y;
+		std::cout << "X: " << (int)player_rb.position_curr.x << ", Y: " << (int)player_rb.position_curr.y << '\n';
+	}
+	else
+		player_rb.velocity.y = 0.0f; // Stop moving the player in the y axis
+
+	// Press 'A' or left key to move the player left
+	if (UME::Input::IsKeyPressed(UME_KEY_A) || UME::Input::IsKeyPressed(UME_KEY_LEFT))
+	{
+		player_rb.velocity.x = -player_rb.acceleration.x;
+		std::cout << "X: " << (int)player_rb.position_curr.x << ", Y: " << (int)player_rb.position_curr.y << '\n';
+	}
+	// Press 'D' or right key to move the player to the right
+	else if (UME::Input::IsKeyPressed(UME_KEY_D) || UME::Input::IsKeyPressed(UME_KEY_RIGHT))
+	{
+		player_rb.velocity.x = player_rb.acceleration.x;
+		std::cout << "X: " << (int)player_rb.position_curr.x << ", Y: " << (int)player_rb.position_curr.y << '\n';
+	}
+	else
+		player_rb.velocity.x = 0.0f; // Stop moving the player in the x axis
+	// --- End User Input ---
+
+	// Update player rb and box collider
+	Ukemochi::Physics::UpdatePhysics(player_rb, player_box_collider);
+
+	// Check collisions
+	float tLast;
+	if (Ukemochi::Collision::CollisionIntersection_BoxBox(player_box_collider, player_rb.velocity, player_box_collider, player_rb.velocity, tLast))
+	{
+		// do collision response
+		//player_rb.position = player_rb.initial_position;
+		//std::cout << "box box collided\n";
+	}
+	//if (Ukemochi::Collision::CollisionIntersection_BoxScreen(player_box_collider, renderer_system.screen_width, renderer_system.screen_height))
+	//{
+	//	//std::cout << "box collided with game screen boundary\n";
+	//}
 }
 
 void Level1_Draw()//rendering of the game for Level1
 {
 	//some code here
 	//std::cout << "Level1:Draw" << '\n';
+	//renderer_system.drawBox(player_rb.position_curr.x + renderer_system.screen_width / 2, player_rb.position_curr.y + renderer_system.screen_height / 2, player_rb.scale.x, player_rb.scale.y, 0);
 }
 
 void Level1_Free()//release unused assets/variable memories
