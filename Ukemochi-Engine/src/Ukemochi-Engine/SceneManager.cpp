6957b66568c255885012105f54b1fbede964d4b8
hansi-sh <tansihan1@gmail.com>
2024-09-27 18:07:46 +0800

    update scene manager

--- Ukemochi-Engine/src/Ukemochi-Engine/SceneManager.cpp
+++ Ukemochi-Engine/src/Ukemochi-Engine/SceneManager.cpp
@@ -1,9 +1,23 @@
 #include "PreCompile.h"
 #include "SceneManager.h"
+#include "Audio/Audio.h"
 
 namespace Ukemochi
 {
+	SceneManager::SceneManager()
+	{
+		Audio audio;
+	}
+
+	SceneManager::~SceneManager()
+	{
+	}
+
 	void SceneManager::LoadScene()
+	{
+	}
+
+	void SceneManager::SaveScene()
 	{
 	}
 
--- Ukemochi-Engine/src/Ukemochi-Engine/SceneManager.h
+++ Ukemochi-Engine/src/Ukemochi-Engine/SceneManager.h
@@ -7,11 +7,16 @@ namespace Ukemochi
 	class SceneManager
 	{
 	public:
+		SceneManager();
+		~SceneManager();
+
 		void LoadScene();
+		void SaveScene();
 		void Update();
 		void ClearScene();
 
 	private:
 
 	};
+
 }
