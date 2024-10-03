e348dc1d5ce85378e791df34698ee9b4ac7f8f39
Kean <junyukean.wong@digipen.edu>
2024-10-04 00:06:33 +0800

    added back Middle box collider

--- Ukemochi-Engine/src/Ukemochi-Engine/Game/Level1.cpp
+++ Ukemochi-Engine/src/Ukemochi-Engine/Game/Level1.cpp
@@ -92,6 +92,8 @@ namespace Ukemochi
 				0,
 				Vec2{SPRITE_SCALE * 1.5f, SPRITE_SCALE * 1.5f}
 			});
+		background.AddComponent(Rigidbody2D{ Vec2{}, Vec2{}, true });
+		background.AddComponent(BoxCollider2D{ Vec2{}, Vec2{} });
 		background.AddComponent(SpriteRender{ "../Assets/Textures/Moon Floor.png" });
 
 		// WORM OBJECT 1 - DYNAMIC
@@ -107,7 +109,6 @@ namespace Ukemochi
 		worm_0.AddComponent(SpriteRender{
 				"../Assets/Textures/Worm.png",
 				SPRITE_SHAPE::BOX,
-				true
 			});
 
 		// WORM OBJECT 2 - DYNAMIC - CLONED
