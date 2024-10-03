1c2b044d1c1731581482e58ff93fb6dc87b40c0a
hansi-sh <tansihan1@gmail.com>
2024-10-04 00:54:43 +0800

    add header to game state list

--- Ukemochi-Engine/src/Ukemochi-Engine/Game/GameStateList.h
+++ Ukemochi-Engine/src/Ukemochi-Engine/Game/GameStateList.h
@@ -1,3 +1,15 @@
+/* Start Header ************************************************************************/
+/*!
+\file    GameStateLish.h
+\author  Tan Si Han, t.sihan, 2301264
+\par     t.sihan@digipen.edu
+\date    20/09/2024
+\brief   This files contains the enum of GameStateList.
+
+
+Copyright (C) 2024 DigiPen Institute of Technology.  Reproduction or disclosure of this file or its contents without the prior written consent of DigiPen Institute of Technology is prohibited.
+*/
+/* End Header **************************************************************************/
 #pragma once
 
 namespace Ukemochi
