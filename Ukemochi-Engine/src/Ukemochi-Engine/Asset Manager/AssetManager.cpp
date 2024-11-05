cf03fd69938a338d4ee1a017f46a4abe823b08b2
oui-balls <p.junkaigerald@digipen.edu>
2024-11-05 14:38:44 +0800

    SHIET

--- Ukemochi-Engine/src/Ukemochi-Engine/Asset Manager/AssetManager.cpp
+++ Ukemochi-Engine/src/Ukemochi-Engine/Asset Manager/AssetManager.cpp
@@ -10,8 +10,20 @@ namespace Ukemochi
 			std::cout << "Texture already exists";
 			return;
 		}
-
 		
+		std::string extention = file_path.substr(file_path.find_last_of('.') + 1);
+		std::transform(extention.begin(), extention.end(), extention.begin(), ::tolower);
+
+		GLenum file_render;
+		if (extention.compare("png") == 0)\
+		{
+			file_render = GL_RGBA;
+		}
+		else if (extention.compare("jpeg") == 0 || extention.compare("jpg") == 0)
+		{
+			file_render = GL_RGB;
+		}
+
 	}
 
 	Texture& AssetManager::getTexture(std::string key_name)
