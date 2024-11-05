7ad655f6705a5e410c5d294d5016236217842a9d
oui-balls <p.junkaigerald@digipen.edu>
2024-11-05 14:14:56 +0800

    trying to use ECS

--- Ukemochi-Engine/src/Ukemochi-Engine/Asset Manager/AssetManager.cpp
+++ Ukemochi-Engine/src/Ukemochi-Engine/Asset Manager/AssetManager.cpp
@@ -2,37 +2,22 @@
 
 namespace Ukemochi
 {
-
-	/*void AssetManager::AddAsset(std::string file_path)
+	
+	void AssetManager::addTexture(std::string file_name, std::string file_path)
 	{
-		size_t start = file_path.find_last_of('\\');
-		size_t end = file_path.find_first_of('.');
-		size_t len = end - start;
-		std::string key = file_path.substr(start, len);
-		Asset created(file_path, key);
-		if (created.GetType() == Invalid)
+		if (texture_list.find(file_name) != texture_list.end())
 		{
+			std::cout << "Texture already exists";
 			return;
 		}
-		asset_list.emplace(key, created);
-		return;
-	}*/
 
-	void AssetManager::loadAssetsFromFolder(std::string folder_path)
-	{
-		for (const auto& entries : std::filesystem::directory_iterator(folder_path))
-		{
-			if (!entries.is_directory()) //entries is a file
-			{
-				//check extention and add asset accordingly
-			}
-			else //entries is a folder
-			{
-				loadAssetsFromFolder(entries.path().generic_string()); //recur function with the subfolder;
-				continue;
-			}
-		}
-		return;
+		
 	}
+
+	Texture& AssetManager::getTexture(std::string key_name)
+	{
+		return *texture_list.find(key_name)->second;
+	}
+
 	
 }
--- Ukemochi-Engine/src/Ukemochi-Engine/Asset Manager/AssetManager.h
+++ Ukemochi-Engine/src/Ukemochi-Engine/Asset Manager/AssetManager.h
@@ -4,35 +4,34 @@
 #include "PreCompile.h"
 #include "../Graphics/Renderer.h"
 #include "../Graphics/Sprite.h"
-#include "../Graphics/shaderClass.h"
 #include "../Graphics/Texture.h"
-#include "../Audio/Audio.h"
+#include "../ECS/ECS.h"
 
 
 namespace Ukemochi
 {
-	class AssetManager
+	class AssetManager : public Ukemochi::System
 	{
 	private:
-		std::map<std::string, Sprite> sprite_list;
-		std::map<std::string, Texture> texture_list;
-		std::map<std::string, Shader> shader_list;
-		std::map<std::string, Audio> audio_list;
+		std::map<std::string, std::shared_ptr<Sprite>> sprite_list;
+		std::map<std::string, std::shared_ptr<Texture>> texture_list;
+		std::map<std::string, std::shared_ptr<Shader>> shader_list;
+		//std::map<std::string, Audio> audio_list;
 
 	public:
-		bool addSprite(std::string file_name, std::string file_path /* can add more if need any other arguements */);
-		Sprite getSprite(std::string key_name);
+		void addSprite(std::string file_name, std::string file_path /* can add more if need any other arguements */);
+		Sprite& getSprite(std::string key_name);
 
-		bool addTexture(std::string file_name, std::string file_path /* can add more if need any other arguements */);
-		Texture getTexture(std::string key_name);
+		void addTexture(std::string file_name, std::string file_path /* can add more if need any other arguements */);
+		Texture& getTexture(std::string key_name);
 
-		bool addShader(std::string file_name, std::string file_path /* can add more if need any other arguements */);
-		Shader getShader(std::string key_name);
+		void addShader(std::string file_name, std::string file_path /* can add more if need any other arguements */);
+		Shader& getShader(std::string key_name);
 
-		bool addAudio(std::string file_name, std::string file_path /* can add more if need any other arguements */);
-		Audio getAudio(std::string kwey_name);
+		//bool addAudio(std::string file_name, std::string file_path /* can add more if need any other arguements */);
+		//Audio getAudio(std::string kwey_name);
 
-		void loadAssetsFromFolder(std::string folder_path);
+		//void loadAssetsFromFolder(std::string folder_path);
 	};
 
 	
