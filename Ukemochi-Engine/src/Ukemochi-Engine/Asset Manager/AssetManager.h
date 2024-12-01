d1be591a776de34c3d7e819f59ddab3734ab633b
Gerald <geraldpek.18@ichat.sp.edu.sg>
2024-12-01 14:10:24 +0800

    Partial migration

--- Ukemochi-Engine/src/Ukemochi-Engine/Asset Manager/AssetManager.h
+++ Ukemochi-Engine/src/Ukemochi-Engine/Asset Manager/AssetManager.h
@@ -28,22 +28,52 @@ namespace Ukemochi
 	private:
 		//std::map<std::string, std::shared_ptr<SpriteRender>> sprite_list;
 		//std::map<std::string, std::shared_ptr<FMOD::Sound>> audio_list;
+
+		size_t texture_list_size;
+		int sound_count;
+		std::vector<std::string> texture_order;
+		std::unordered_map<std::string, std::shared_ptr<Texture>> texture_list;
+		std::unordered_map<std::string, std::shared_ptr<Shader>> shader_list;
+		std::vector<std::shared_ptr<FMOD::Sound>> sound_list;
+		std::vector<std::string> sound_name_list;
+		std::string asset_dir{ "../Assets" };
+		//try to store game object prefab here
+
 	public:
 		//void addSprite(std::string file_name, std::string file_path /* can add more if need any other arguements */);
 		//SpriteRender& getSprite(std::string key_name);
 
-		void addTexture(std::string filePath, size_t& orderIndex);
+		AssetManager();
+		~AssetManager();
+
+		void addTexture(std::string file_path);
 		std::shared_ptr<Texture> getTexture(std::string key_name);
 
 		void addShader(std::string file_name, std::string vert_path, std::string frag_path);
 		std::shared_ptr<Shader> getShader(std::string key_name);
 
-		size_t order_index{};
-		std::vector<std::string> texture_order;
-		std::map<std::string, std::shared_ptr<Texture>> texture_list;
-		std::map<std::string, std::shared_ptr<Shader>> shader_list;
+		void addSound(std::string file_path);
+		std::shared_ptr<FMOD::Sound> getSound(int key);
 
+		//addPrefab
+		//getPrefab
+
+		bool ifTextureExists(std::string key_name);
+
+		size_t getTextureOrderSize();
+
+		const size_t getTextureListSize() const;
+
+		std::string& getTextureAtIndex(int index);
+
+		void loadAssetsFromFolder();
+
+		static AssetManager& initASM()
+		{
+			static std::unique_ptr<AssetManager>  manager(new AssetManager());
+			return *manager;
+		}
 	};
 
-	
+
 }
