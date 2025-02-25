30919530ce7009c29db0d87d3846af2b51290781
Lum Ko Sand <42332579+dnasok@users.noreply.github.com>
2025-02-25 17:57:11 +0800

    Implement shadow depth scaling and pair floating soul and shadow with player for layering

--- Ukemochi-Engine/src/Ukemochi-Engine/Math/Transformation.h
+++ Ukemochi-Engine/src/Ukemochi-Engine/Math/Transformation.h
@@ -2,7 +2,7 @@
 /*!
 \file       Transformation.h
 \author     Lum Ko Sand, kosand.lum, 2301263, kosand.lum\@digipen.edu
-\date       Feb 21, 2025
+\date       Feb 25, 2025
 \brief      This file contains the declaration of the Transformation system.
 
 Copyright (C) 2024 DigiPen Institute of Technology.
@@ -34,19 +34,23 @@ namespace Ukemochi
     private:
         /*!***********************************************************************
         \brief
-         Minimum and maximum scale constant for all entities.
+         Minimum and maximum Y position and scale constant for all entities.
         *************************************************************************/
         const float MIN_Y_POS = 100.0f, MAX_Y_POS = 800.0f;
         const float MIN_SCALE = 150.0f, MAX_SCALE = 200.0f;
         const float SCALE_FACTOR = 100.f;
+        const DepthScaling OBJECT_SCALING = { MIN_Y_POS, MAX_Y_POS, MIN_SCALE, MAX_SCALE }; // Object scaling constant for the depth effect.
 
-        /*!***********************************************************************
-        \brief
-         Object scaling constant for the depth effect.
-        *************************************************************************/
-        const DepthScaling OBJECT_SCALING = { MIN_Y_POS, MAX_Y_POS, MIN_SCALE, MAX_SCALE };
+        EntityID soul;   // Keep track of the floating soul ID
+        EntityID shadow; // Keep track of the player's shadow ID
 
     public:
+        /*!***********************************************************************
+        \brief
+         Initialize the transformation system.
+        *************************************************************************/
+        void Init();
+
         /*!***********************************************************************
         \brief
          Compute the transformations of all the entities.
