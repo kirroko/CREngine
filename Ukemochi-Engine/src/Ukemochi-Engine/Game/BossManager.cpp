3d828937808e97c2d7021cc55938148e151f5bfa
hansi-sh <tansihan1@gmail.com>
2025-03-09 17:31:33 +0800

    set up boss

--- Ukemochi-Engine/src/Ukemochi-Engine/Game/BossManager.cpp
+++ Ukemochi-Engine/src/Ukemochi-Engine/Game/BossManager.cpp
@@ -1,5 +1,47 @@
 #include "PreCompile.h"
 #include "BossManager.h"
+#include "../Factory/GameObjectManager.h"
+
 namespace Ukemochi
 {
+	void BossManager::InitBoss()
+	{
+
+		hair = GameObjectManager::GetInstance().GetGOByTag("Hair");
+		blob = GameObjectManager::GetInstance().GetGOByTag("Blob");
+		hairHitBox = GameObjectManager::GetInstance().GetGOByTag("HitBox");
+		playerObj = GameObjectManager::GetInstance().GetGOByTag("Player");
+		boss = GameObjectManager::GetInstance().GetGOByTag("Boss")->GetComponent<Boss>();
+	}
+	void BossManager::UpdateBoss()
+	{
+		if (boss.BossPhase == 1)
+		{
+			if (boss.health <= 0)
+			{
+				boss.BossPhase = 2;
+			}
+			else
+			{
+				Phase1();
+			}
+		}
+		else
+		{
+			if (boss.health <= 0)
+			{
+				//WIN
+			}
+			else
+			{
+				Phase2();
+			}
+		}
+	}
+	void BossManager::Phase1()
+	{
+	}
+	void BossManager::Phase2()
+	{
+	}
 }
--- Ukemochi-Engine/src/Ukemochi-Engine/Game/BossManager.h
+++ Ukemochi-Engine/src/Ukemochi-Engine/Game/BossManager.h
@@ -1,10 +1,25 @@
 #pragma once
 #include "../ECS/ECS.h"
+#include "../Factory/GameObject.h"
 
 namespace Ukemochi
 {
 	class BossManager : public System
 	{
+	public:
+		void InitBoss();
 
+		void UpdateBoss();
+
+		void Phase1();
+
+		void Phase2();
+
+	private:
+		GameObject* playerObj;
+		GameObject* hair;
+		GameObject* hairHitBox;
+		GameObject* blob;
+		Boss boss;
 	};
 }
