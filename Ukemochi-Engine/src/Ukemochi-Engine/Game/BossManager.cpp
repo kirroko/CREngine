58c34278c3ea783c6bb3340515c503defeec0589
hansi-sh <tansihan1@gmail.com>
2025-03-18 18:35:12 +0800

    add headerr

--- Ukemochi-Engine/src/Ukemochi-Engine/Game/BossManager.cpp
+++ Ukemochi-Engine/src/Ukemochi-Engine/Game/BossManager.cpp
@@ -1,3 +1,16 @@
+/* Start Header ************************************************************************/
+/*!
+\file       BossManager.cpp
+\author     Tan Si Han, t.sihan, 2301264, t.sihan@digipen.edu (90%)
+\co-authors HURNG Kai Rui, h.kairui, 2301278, h.kairui\@digipen.edu (10%)
+\date       Feb 05, 2024
+\brief      This file contains the definition of the BossManager class and related methods.
+
+Copyright (C) 2024 DigiPen Institute of Technology.
+Reproduction or disclosure of this file or its contents without the
+prior written consent of DigiPen Institute of Technology is prohibited.
+*/
+/* End Header **************************************************************************/
 #include "PreCompile.h"
 #include "BossManager.h"
 #include "../Factory/GameObjectManager.h"
