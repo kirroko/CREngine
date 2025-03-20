b1025502783fb19913b783fcd6304ee7ebfe914e
hansi-sh <tansihan1@gmail.com>
2025-03-20 15:31:15 +0800

    wip boss

--- Ukemochi-Engine/src/Ukemochi-Engine/Game/BossManager.cpp
+++ Ukemochi-Engine/src/Ukemochi-Engine/Game/BossManager.cpp
@@ -24,20 +24,34 @@ namespace Ukemochi
 	{
 
 		hair = GameObjectManager::GetInstance().GetGOByTag("Hair");
-		hair->GetComponent<Animation>().SetAnimation("HairAtk");
+		if (hair && hair->HasComponent<Animation>())
+		{
+			hair->GetComponent<Animation>().SetAnimation("HairAtk");
+		}
+
 		hairHitBox = GameObjectManager::GetInstance().GetGOByTag("HitBox");
-		hairPosX = hair->GetComponent<Transform>().position.x;
+		if (hairHitBox)
+		{
+			hairPosX = hair->GetComponent<Transform>().position.x;
+		}
 
 		blob = GameObjectManager::GetInstance().GetGOByTag("BlobClone");
-		blob->GetComponent<Animation>().SetAnimation("Idle");
-		blobsize = blob->GetComponent<Transform>().scale.x;
-		blob->GetComponent<Transform>().scale = Vec2();
+		if (blob && blob->HasComponent<Animation>())
+		{
+			blob->GetComponent<Animation>().SetAnimation("Idle");
+			blobsize = blob->GetComponent<Transform>().scale.x;
+			blob->GetComponent<Transform>().scale = Vec2();
+		}
+
 		spawnBlob = false;
 		numOfBlob = 0;
 
 		playerObj = GameObjectManager::GetInstance().GetGOByTag("Player");
 		boss = GameObjectManager::GetInstance().GetGOByTag("Boss");
-		bossCom = GameObjectManager::GetInstance().GetGOByTag("Boss")->GetComponent<Boss>();
+		if (boss && boss->HasComponent<Boss>())
+		{
+			bossCom = GameObjectManager::GetInstance().GetGOByTag("Boss")->GetComponent<Boss>();
+		}
 	}
 	void BossManager::UpdateBoss()
 	{
@@ -45,32 +59,40 @@ namespace Ukemochi
 			return;
 		for (int step = 0; step < g_FrameRateController.GetCurrentNumberOfSteps(); ++step)
 		{
+			if (!boss || !playerObj)
+			{
+				return;
+			}
+
 			if (bossCom.BossPhase == 1)
 			{
-				hair->SetActive(false);
-				hairHitBox->SetActive(false);
-				//CHANGE TO IF THERE NO ENEMY LEFT AND NUM OF BLOB MORE THAN 2
-				if (!ECS::GetInstance().GetSystem<DungeonManager>()->enemy_alive && numOfBlob >= 4)
+				if (bossCom.BossPhase == 1)
 				{
-					//play animation
-					GameObjectManager::GetInstance().GetGOByTag("Boss")->GetComponent<Animation>().SetAnimationUninterrupted("Change");
-					//animation finish then proceed
-					bossCom.BossPhase = 2;
+					hair->SetActive(false);
+					hairHitBox->SetActive(false);
+					// CHANGE TO IF THERE NO ENEMY LEFT AND NUM OF BLOB MORE THAN 2
+					if (!ECS::GetInstance().GetSystem<DungeonManager>()->enemy_alive && numOfBlob >= 4)
+					{
+						// play animation
+						GameObjectManager::GetInstance().GetGOByTag("Boss")->GetComponent<Animation>().SetAnimationUninterrupted("Change");
+						// animation finish then proceed
+						bossCom.BossPhase = 2;
+					}
+					else
+					{
+						Phase1();
+					}
 				}
 				else
 				{
-					Phase1();
-				}
-			}
-			else
-			{
-				if (bossCom.health <= 0)
-				{
-					//WIN
-				}
-				else
-				{
-					Phase2();
+					if (bossCom.health <= 0)
+					{
+						// WIN
+					}
+					else
+					{
+						Phase2();
+					}
 				}
 			}
 		}
@@ -78,8 +100,8 @@ namespace Ukemochi
 	void BossManager::Phase1()
 	{
 		GameObjectManager::GetInstance().GetGOByTag("Boss")->GetComponent<Animation>().SetAnimation("Idle1");
-		//attack
-		//timer end play attack
+		// attack
+		// timer end play attack
 		if (bossCom.waitTime < 7.f)
 		{
 			bossCom.waitTime += static_cast<float>(g_FrameRateController.GetFixedDeltaTime());
@@ -89,22 +111,22 @@ namespace Ukemochi
 			if (numOfBlob <= 3)
 			{
 				spawnBlob = true;
-				//play animation
+				// play animation
 				GameObjectManager::GetInstance().GetGOByTag("Boss")->GetComponent<Animation>().SetAnimationUninterrupted("Change");
 				numOfBlob++;
 			}
 		}
-		//spawn blob blob
-		//scale from 0 - orignal size
+		// spawn blob blob
+		// scale from 0 - orignal size
 		if (spawnBlob && numOfBlob <= 3)
 		{
-			//spawn blob blob
+			// spawn blob blob
 			static int number = 0;
-			GameObject* cloneObject = GameObjectManager::GetInstance().GetGOByTag("BlobClone");
+			GameObject *cloneObject = GameObjectManager::GetInstance().GetGOByTag("BlobClone");
 			if (cloneObject != nullptr)
 			{
 				std::string name = "blob" + std::to_string(number++);
-				GameObject& newObject = GameObjectManager::GetInstance().CloneObject(*cloneObject, name, "Blob");
+				GameObject &newObject = GameObjectManager::GetInstance().CloneObject(*cloneObject, name, "Blob");
 
 				newObject.GetComponent<Transform>().position.x = playerObj->GetComponent<Transform>().position.x;
 				newObject.GetComponent<Transform>().position.y = playerObj->GetComponent<Transform>().position.y;
@@ -112,7 +134,6 @@ namespace Ukemochi
 			spawnBlob = false;
 			bossCom.waitTime = 0;
 		}
-
 	}
 	void BossManager::Phase2()
 	{
@@ -120,29 +141,28 @@ namespace Ukemochi
 		static float delay = 0.f;
 		static float atkdelay = 0.f;
 		static bool atk = false;
-		//hair attack
+		// hair attack
 		if (delay < 7.f)
 		{
 			delay += static_cast<float>(g_FrameRateController.GetFixedDeltaTime());
-		}
-		else
-		{
-			if (!atk)
+
+			if (delay > 6.f)
 			{
-				//set hair attack
-				if (playerObj->GetComponent<Transform>().position.x > boss->GetComponent<Transform>().position.x)
+				hair->SetActive(false);
+				// set hair attack
+				if (playerObj->GetComponent<Transform>().position.x > boss->GetComponent<Transform>().position.x + 50.f)
 				{
-					//right
+					// right
 					hair->GetComponent<SpriteRender>().flipX = true;
 					hairHitBox->GetComponent<Transform>().scale.x = -853.000f;
 
-					hair->GetComponent<Transform>().position.x = 9375;
-					hairHitBox->GetComponent<Transform>().position.x = 9200;
+					hair->GetComponent<Transform>().position.x = 9325;
+					hairHitBox->GetComponent<Transform>().position.x = 9150;
 					hairPosX = hair->GetComponent<Transform>().position.x;
 				}
 				else
 				{
-					//left
+					// left
 					hair->GetComponent<SpriteRender>().flipX = false;
 					hairHitBox->GetComponent<Transform>().position.x = 8100;
 
@@ -150,14 +170,20 @@ namespace Ukemochi
 					hairHitBox->GetComponent<Transform>().scale.x = 853.000f;
 					hairPosX = hair->GetComponent<Transform>().position.x;
 				}
+				hair->GetComponent<Transform>().position.y = playerObj->GetComponent<Rigidbody2D>().position.y;
+				hairHitBox->GetComponent<Transform>().position.y = hair->GetComponent<Transform>().position.y - 50.f;
+
+				hairHitBox->SetActive(true);
+			}
+		}
+		else
+		{
+			if (!atk)
+			{
 				hair->SetActive(true);
 				hair->GetComponent<Animation>().RestartAnimation();
 
-				hair->GetComponent<Transform>().position.y = playerObj->GetComponent<Rigidbody2D>().position.y;
-				hairHitBox->GetComponent<Transform>().position.y = hair->GetComponent<Transform>().position.y-50.f;
-				hairHitBox->SetActive(true);
 				atk = true;
-
 			}
 		}
 
@@ -178,7 +204,7 @@ namespace Ukemochi
 					{
 						if (hair->GetComponent<Transform>().position.x > hairPosX - 1000.f)
 						{
-							hair->GetComponent<Transform>().position.x -= 1;
+							hair->GetComponent<Transform>().position.x -= 150 * g_FrameRateController.GetFixedDeltaTime();
 						}
 						else
 						{
@@ -191,7 +217,7 @@ namespace Ukemochi
 					{
 						if (hair->GetComponent<Transform>().position.x < hairPosX + 1000.f)
 						{
-							hair->GetComponent<Transform>().position.x += 1;
+							hair->GetComponent<Transform>().position.x += 150 * g_FrameRateController.GetFixedDeltaTime();
 						}
 						else
 						{
@@ -200,30 +226,24 @@ namespace Ukemochi
 							atkdelay = 0.f;
 						}
 					}
-
 				}
-
 			}
 		}
 
-		//if (hair->GetComponent<Animation>().GetCurrentFrame() == 33)
+		// if (hair->GetComponent<Animation>().GetCurrentFrame() == 33)
 		//{
 		//	std::cout << "RESTART" << std::endl;
 		//	hair->GetComponent<Animation>().RestartAnimation();
-		//}
-
-
-
-
+		// }
 	}
 	void BossManager::SpawnMonster(float x, float y)
 	{
-		//spawn monster
+		// spawn monster
 		static int number = 0;
 
 		for (int i = 0; i < 3; i++)
 		{
-			GameObject* cloneObject;
+			GameObject *cloneObject;
 			if (numOfBlob % 2 == 0)
 			{
 				cloneObject = GameObjectManager::GetInstance().GetGOByTag("Enemy");
@@ -238,8 +258,8 @@ namespace Ukemochi
 				std::string name = "6_Enemy" + std::to_string(number++);
 				std::string name2 = name + "_Shadow";
 
-				GameObject& newObject = GameObjectManager::GetInstance().CloneObject(*cloneObject, name, "Enemy");
-				GameObject& shadow = GameObjectManager::GetInstance().CloneObject(*cloneObject, name2, "EnemyShadow");
+				GameObject &newObject = GameObjectManager::GetInstance().CloneObject(*cloneObject, name, "Enemy");
+				GameObject &shadow = GameObjectManager::GetInstance().CloneObject(*cloneObject, name2, "EnemyShadow");
 
 				newObject.GetComponent<Transform>().position.x = x;
 				newObject.GetComponent<Transform>().position.y = y;
@@ -252,7 +272,7 @@ namespace Ukemochi
 					}
 					else
 					{
-						newObject.AddComponent(Enemy{ x, y, Enemy::FISH, newObject.GetInstanceID() });
+						newObject.AddComponent(Enemy{x, y, Enemy::FISH, newObject.GetInstanceID()});
 					}
 
 					if (shadow.HasComponent<SpriteRender>())
@@ -262,7 +282,7 @@ namespace Ukemochi
 					}
 					else
 					{
-						SpriteRender sr = { "../Assets/Textures/Fish_Shadow.png", SPRITE_SHAPE::BOX, 1 };
+						SpriteRender sr = {"../Assets/Textures/Fish_Shadow.png", SPRITE_SHAPE::BOX, 1};
 						shadow.AddComponent<SpriteRender>(sr);
 					}
 				}
@@ -274,7 +294,7 @@ namespace Ukemochi
 					}
 					else
 					{
-						newObject.AddComponent(Enemy{ x, y, Enemy::WORM, newObject.GetInstanceID() });
+						newObject.AddComponent(Enemy{x, y, Enemy::WORM, newObject.GetInstanceID()});
 					}
 
 					if (shadow.HasComponent<SpriteRender>())
@@ -284,14 +304,13 @@ namespace Ukemochi
 					}
 					else
 					{
-						SpriteRender sr = { "../Assets/Textures/Worm_shadow.png", SPRITE_SHAPE::BOX, 1 };
+						SpriteRender sr = {"../Assets/Textures/Worm_shadow.png", SPRITE_SHAPE::BOX, 1};
 						shadow.AddComponent<SpriteRender>(sr);
 					}
 				}
 				ECS::GetInstance().GetSystem<DungeonManager>()->rooms[6].enemies.push_back(newObject.GetInstanceID());
 				ECS::GetInstance().GetSystem<DungeonManager>()->rooms[6].entities.push_back(newObject.GetInstanceID());
 			}
-		
 		}
 
 		ECS::GetInstance().GetSystem<EnemyManager>()->UpdateEnemyList();
